f06ea9812ae2be6e1694eb3f9d565c6efa0026f2
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-03 23:12:21 -0600

    Add feature: delete a shelter

--- spec/features/shelters/delete_spec.rb
+++ spec/features/shelters/delete_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe "Shelter Delete Spec" do
   describe "As a visitor" do
     describe "When I visit a shelter show page" do
-      it "" do
+      it "I can delete that shelter" do
         catto_house = Shelter.create(name: "Catto House", address: "2124 N. Pencil Ave", city: "Denver", state: "CO", zip: "000000")
         liono_house = Shelter.create(name: "Liono House", address: "6126 Library Blvd", city: "Denver", state: "CO", zip: "000000")
-        id = liono_house.id
-        visit "/shelters/#{id}"
+
+        visit "/shelters/#{liono_house.id}"
 
         click_link "Delete Shelter"
         expect(current_path).to eq("/shelters")
@@ -17,6 +17,5 @@ describe "Shelter Delete Spec" do
       end
     end
   end
-# and I am redirected to the shelter index page where I no longer see this shelter
 
 end

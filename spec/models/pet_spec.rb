38023c6c587f5e5ecd5ef2bd3258977f91c092c2
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-04 13:23:25 -0600

    Update Pet Model spec to have descriptions and adoptability status

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -10,5 +10,7 @@ describe Pet, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:sex) }
     it { should validate_numericality_of(:approximate_age).is_greater_than_or_equal_to(0) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:status) }
   end
 end

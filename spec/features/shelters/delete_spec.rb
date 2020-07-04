173f9451800e9a3bfe97bbb36fa1cc0653a84506
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-03 22:36:03 -0600

    Add more setup to shelters/delete spec

--- spec/features/shelters/delete_spec.rb
+++ spec/features/shelters/delete_spec.rb
@@ -4,6 +4,7 @@ describe "Shelter Delete Spec" do
   describe "As a visitor" do
     describe "When I visit a shelter show page" do
       it "" do
+        catto_house = Shelter.create(name: "Catto House", address: "2124 N. Pencil Ave", city: "Denver", state: "CO", zip: "000000")
         liono_house = Shelter.create(name: "Liono House", address: "6126 Library Blvd", city: "Denver", state: "CO", zip: "000000")
         id = liono_house.id
         visit "/shelters/#{id}"
@@ -11,7 +12,8 @@ describe "Shelter Delete Spec" do
         click_link "Delete Shelter"
         expect(current_path).to eq("/shelters")
 
-        expect(page).to have_no_text("Liono House")
+        expect(page).to have_no_text(liono_house.name)
+        expect(page).to have_text(catto_house.name)
       end
     end
   end

343db2b2717b2dc396b49a986427a8fdb01cb185
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-04 11:18:26 -0600

    Fix Pet index spec to use expect - include

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -4,6 +4,7 @@ describe "Pet Index Spec" do
   describe "As a visitor to '/pets'" do
     it "I can see each Pet's image, name, approx. age, sex, and shelter name" do
       placeholder_image = "generic-image-placeholder.png"
+      image_name = placeholder_image[0...-4]
 
       doggo_house = Shelter.create(name: "Doggo House", address: "1323 Paper St", city: "Denver", state: "CO", zip: "000000")
       catto_house = Shelter.create(name: "Catto House", address: "2124 N. Pencil Ave", city: "Denver", state: "CO", zip: "000000")
@@ -14,8 +15,8 @@ describe "Pet Index Spec" do
       visit "/pets"
 
       doggo_image = find("#doggo-image")
-      expect(doggo_image[:src]).to eq(placeholder_image)
-      expect(doggo_image[:src]).to eq("#{doggo.name}'s photo.")
+      expect(doggo_image[:src]).to include(image_name)
+      expect(doggo_image[:alt]).to eq("#{doggo.name}'s photo.")
 
       expect(page).to have_text(doggo.name)
       expect(page).to have_text(doggo.approximate_age)
@@ -23,8 +24,8 @@ describe "Pet Index Spec" do
       expect(page).to have_text(doggo_house.name)
 
       catto_image = find("#catto-image")
-      expect(catto_image[:src]).to eq(placeholder_image)
-      expect(catto_image[:src]).to eq("#{catto.name}'s photo.")
+      expect(catto_image[:src]).to include(image_name)
+      expect(catto_image[:alt]).to eq("#{catto.name}'s photo.")
 
       expect(page).to have_text(catto.name)
       expect(page).to have_text(catto.approximate_age)

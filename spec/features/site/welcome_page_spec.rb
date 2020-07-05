bf5f31c3638481910b3135f91b118f8c9174756c
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-05 17:27:52 -0600

    Add specs for home page link on every page

--- spec/features/site/welcome_page_spec.rb
+++ spec/features/site/welcome_page_spec.rb
@@ -2,6 +2,11 @@ require "rails_helper"
 
 describe "The Welcome Page" do
   describe "When I visit the home page" do
+    it "has a link to the home page" do
+      visit "/"
+
+      expect(page).to have_link("Home Page", href: "/")
+    end
     it "has links to a Pets and Shelter indices" do
       visit "/"
 
